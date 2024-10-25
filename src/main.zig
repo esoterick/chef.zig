1503d862094a6026b190cc226b2f6a0f4fbda2e6
r0b <r0b@syn-2603-7080-8f02-2b30-0000-0000-0000-0323.res6.spectrum.com>
2024-10-25 08:21:02 -0400

    feat: initial commit

---
+++ src/main.zig
@@ -0,0 +1,39 @@
+// main.zig - Main entrypoint for chef.zig
+// Copyright (C) 2024 - Robert J. Lambert III <rob@rlambert3.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+const std = @import("std");
+const auth = @import("auth.zig");
+
+pub fn main() !void {
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    // stdout is for the actual output of your application, for example if you
+    // are implementing gzip, then only the compressed bytes should be sent to
+    // stdout, not any debugging messages.
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    const data: [:0]const u8 = "balls";
+    const hash = try auth.sha1(data);
+
+    try stdout.print("{x}\n", .{hash});
+
+    try bw.flush(); // Don't forget to flush!
+}
---
+++ src/root.zig
@@ -0,0 +1,26 @@
+// root.zig - root library for chef.zig
+// Copyright (C) 2024 - Robert J. Lambert III <rob@rlambert3.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+const std = @import("std");
+const testing = std.testing;
+
+export fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+test "basic add functionality" {
+    try testing.expect(add(3, 7) == 10);
+}
